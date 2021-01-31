bc37adb4aeff22a6b6e1730ae680539390f7d6ae
takahiro-hayakawa <t.hayakawa1200@gmail.com>
2021-01-31 15:15:43 +0900

    gin導入

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/takahiro-hayakawa/user-api-server
 
 go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
 
 func main() {
+	r := gin.Default()
 	iUserRepository := NewUserRepository()
 	userService := NewUserService(iUserRepository)
 	userApplicationService := NewUserApplicationService(iUserRepository, userService)
-	err := userApplicationService.Register("takahiro")
-	if err != nil {
-		panic(err)
-	}
-	userDTO := userApplicationService.Find(1)
-	fmt.Printf("userID:%d\n", userDTO.ID)
-	fmt.Printf("userID:%s\n", userDTO.Name)
-	fmt.Printf("userID:%s\n", userDTO.MailAddress)
+
+	r.GET("/user/:id", func(c *gin.Context) {
+		idStr := c.Param("id")
+		id, _ := strconv.Atoi(idStr)
+		userDTO := userApplicationService.Find(id)
+
+		c.JSON(200, gin.H{
+			"user_id":      id,
+			"name":         userDTO.Name,
+			"maid_address": userDTO.MailAddress,
+		})
+	})
+
+	r.POST("/user/", func(c *gin.Context) {
+		userName := c.Param("name")
+		userApplicationService.Register(userName)
+
+		c.JSON(200, gin.H{
+			"message": "success",
+		})
+	})
+
+	r.Run(":3000")
 }
